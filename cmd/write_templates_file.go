fb66e39ba4f4638e53da9f521ecac2249ff3daac
amarantec <marcosamarante.c@gmail.com>
2024-12-23 00:11:59 -0300

    fix path to execute project in /app dir

--- cmd/write_templates_file.go
+++ cmd/write_templates_file.go
@@ -17,7 +17,7 @@ var Templates *template.Template
 
 func LoadTemplates() {
   var err error
-  Templates, err = template.ParseGlob("../../web/templates/*html")
+  Templates, err = template.ParseGlob("./web/templates/*html")
   if err != nil {
     log.Fatalf("Error loading templates: %v\n", err)
   }
