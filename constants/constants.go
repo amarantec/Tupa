f327122c1ba0d30e5df88046f7509e840084894f
amarantec <marcoscarvalhop@outlook.com>
2024-12-30 12:54:09 -0300

    aux constants to remove magic numbers etc

---
+++ constants/constants.go
@@ -0,0 +1,12 @@
+package constants
+
+const (
+	EMPTY_STRING      = ""
+	ZERO              = 0
+	DEFAULT_DB        = "sqlite3"
+	PROJECT_NAME      = "projectName"
+	SERVICE_SUFFIX    = "_service.go"
+	REPOSITORY_SUFFIX = "_repository.go"
+	HANDLER_SUFFIX    = "_handler.go"
+	USAGE_MESSAGE     = "usage: tupa generate -n <ModelName> <Field:Type> <Field:Type>"
+)
