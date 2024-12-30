5df0a7e3685e660d5be55b3f9dd97369f30ab606
amarantec <marcoscarvalhop@outlook.com>
2024-12-30 12:48:45 -0300

    refactor cdoe

--- cmd/generate.go
+++ cmd/generate.go
@@ -9,63 +9,94 @@ import (
 
 	"github.com/amarantec/tupa/cmd/generate"
 	"github.com/amarantec/tupa/cmd/utils"
+	"github.com/amarantec/tupa/constants"
 )
 
+// Constantes para valores padrão
+
+// Função para gerar um novo modelo
 func generateNewModel(args []string) error {
 	if len(args) < 2 {
 		fmt.Println(args)
-		return fmt.Errorf("usage: tupa generate -n <ModelName> <Field:Type> <Field:Type>")
+		return fmt.Errorf(constants.USAGE_MESSAGE)
 	}
 
+	// Gera a estrutura do modelo
 	if err := modelNewStruct(args); err != nil {
 		return err
 	}
 
+	// Variáveis de configuração
 	modelName := args[0]
 	internalPath, err := utils.FindProjectInternal()
 	if err != nil {
 		return err
 	}
 
-	projectName, err = utils.LoadProjectNameFromGoMod()
+	projectName, err := utils.LoadProjectNameFromGoMod()
 	if err != nil {
 		return fmt.Errorf("failed to get module name from go.mod")
 	}
 
-	dbDrive, err = utils.GetDBDriverFromEnv()
+	dbDriver, err := utils.GetDBDriverFromEnv()
 	if err != nil {
 		return err
 	}
 
+	// Caminho do modelo e criação do diretório
 	modelPath := filepath.Join(internalPath, modelName)
 	if err := os.Mkdir(modelPath, os.ModePerm); err != nil {
 		log.Fatal(err)
 	}
 
-	modelServiceFileName := strings.ToLower(modelName + "_service.go")
+	// Gerar arquivos do modelo
+	if err := generateModelFiles(modelName, modelPath, projectName, dbDriver); err != nil {
+		log.Fatal(err)
+	}
+
+	// Gerar SQL para migração
+	sql, err := GenerateSQLFromStruct(args, dbDriver)
+	if err != nil {
+		return fmt.Errorf("failed to generate SQL from struct: %w", err)
+	}
+
+	// Criar migração
+	if err := CreateMigrations(modelName, sql); err != nil {
+		return fmt.Errorf("failed to create migration: %w", err)
+	}
+
+	return nil
+}
+
+// Função auxiliar para gerar os arquivos do modelo
+func generateModelFiles(modelName, modelPath, projectName, dbDriver string) error {
+	// Gerar arquivo de serviço
+	modelServiceFileName := strings.ToLower(modelName + constants.SERVICE_SUFFIX)
 	modelServicePath := filepath.Join(modelPath, modelServiceFileName)
 	modelServiceFile, err := os.Create(modelServicePath)
 	if err != nil {
-		log.Fatal(err)
+		return err
 	}
 	defer modelServiceFile.Close()
 
 	if err := generate.GenerateModelServiceFile(modelServicePath, modelName, projectName); err != nil {
-		log.Fatal(err)
+		return err
 	}
 
-	modelRepositoryFileName := strings.ToLower(modelName + "_repository.go")
+	// Gerar arquivo de repositório
+	modelRepositoryFileName := strings.ToLower(modelName + constants.REPOSITORY_SUFFIX)
 	modelRepositoryPath := filepath.Join(modelPath, modelRepositoryFileName)
 	modelRepositoryFile, err := os.Create(modelRepositoryPath)
 	if err != nil {
-		log.Fatal(err)
+		return err
 	}
 	defer modelRepositoryFile.Close()
 
-	if err := generate.GenerateModelRepositoryFile(modelRepositoryPath, modelName, projectName, dbDrive); err != nil {
-		log.Fatal(err)
+	if err := generate.GenerateModelRepositoryFile(modelRepositoryPath, modelName, projectName, dbDriver); err != nil {
+		return err
 	}
 
+	// Gerar arquivo de handler
 	handlerPath, err := utils.FindProjectHandler()
 	if err != nil {
 		return err
@@ -74,24 +105,19 @@ func generateNewModel(args []string) error {
 	modelHandler := filepath.Join(handlerPath, modelName)
 	modelHandlerDirName := modelHandler + "Handler"
 	if err := os.Mkdir(modelHandlerDirName, os.ModePerm); err != nil {
-		log.Fatal(err)
+		return err
 	}
 
-	modelHandlerFileName := strings.ToLower(modelName + "_handler.go")
+	modelHandlerFileName := strings.ToLower(modelName + constants.HANDLER_SUFFIX)
 	modelHandlerPath := filepath.Join(modelHandlerDirName, modelHandlerFileName)
 	modelHandlerFile, err := os.Create(modelHandlerPath)
 	if err != nil {
-		log.Fatal(err)
+		return err
 	}
 	defer modelHandlerFile.Close()
 
 	if err := generate.GenerateModelHandlerFile(modelHandlerPath, modelName, projectName); err != nil {
-		log.Fatal(err)
-	}
-
-	migrationsPath := filepath.Join(internalPath, "migrations")
-	if err := os.Mkdir(migrationsPath, os.ModePerm); err != nil {
-		log.Fatal(err)
+		return err
 	}
 
 	return nil
